11666a0e7f5f03fec7e2473be3e1311dab5a8ac9
MTardchild <Marius.Tuschl@googlemail.com>
2015-04-02 15:40:29 +0200

    Dynamic Calculation, Random Number Gen

--- main.c
+++ main.c
@@ -1,14 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include "main.h"
 
 int main()
 {
-	int MagicSquare[3][3] = {{2,9,4},
-							 {7,5,3},
-							 {6,1,8}};
+    int MagicSquareSize = 0;
+    int *MagicSquareSizePointer = &MagicSquareSize;
 
-	int size = sizeof(MagicSquare[0])/sizeof(int);
+    printf("Please enter size of square: ");
+    scanf("%d", MagicSquareSizePointer);
 
+	int **MagicSquare = InitializeMagicSquare(MagicSquareSize);
+	int NumberSum = CalculateNumberSum(MagicSquareSize);
+
+    MagicSquare[0][0] = 4;
+    MagicSquare[0][1] = 9;
+    MagicSquare[0][2] = 2;
+
+    MagicSquare[1][0] = 3;
+    MagicSquare[1][1] = 5;
+    MagicSquare[1][2] = 7;
+
+    MagicSquare[2][0] = 8;
+    MagicSquare[2][1] = 1;
+    MagicSquare[2][2] = 6;
+
+    DrawMagicSquare(MagicSquare, MagicSquareSize);
+    CheckMagicSquare(MagicSquare, MagicSquareSize, NumberSum);
+
+    scanf("%d", MagicSquareSizePointer);
+
+	return 0;
+}
+
+void CheckMagicSquare(int **magicSquare, int magicSquareSize, int numberSum) {
 	// Counter Variables
 	int i = 0;
 	int j = 0;
@@ -20,47 +47,113 @@ int main()
 	int Diagonal = 0;
 	int ConDiagonal = 0;
 
-	for (i = 0; i < size; i++) {
-		for (j = 0; j < size; j++) {
+	for (i = 0; i < magicSquareSize; i++) {
+		for (j = 0; j < magicSquareSize; j++) {
 			// Check hor
-			if (((Horizontal += MagicSquare[i][j]) != 15) && j == size-1) {
-				printf("No magic happening...");
-				return 1;
+			if (((Horizontal += magicSquare[i][j]) != numberSum) && j == magicSquareSize-1) {
+				printf("No magic happening...H");
+				return;
 			}
 
 			// Check vert
-			if (((Vertical += MagicSquare[j][i]) != 15) && j == size-1) {
-				printf("No magic happening...");
-				return 1;
+			if (((Vertical += magicSquare[j][i]) != numberSum) && j == magicSquareSize-1) {
+				printf("No magic happening...V");
+				return;
 			}
 
-			if (k < size) {
-				// Check diag
-				if (((Diagonal += MagicSquare[k][j]) != 15) && j == size-1) {
-					printf("%d", Diagonal);
+			if (k < magicSquareSize) {
+                // Check diag
+				if (((Diagonal += magicSquare[k][j]) != numberSum) && j == magicSquareSize-1) {
 					printf("No magic happening...D");
-					return 1;
-			}
+					return;
+                }
 
-			// Check condiag
-			if (((ConDiagonal += MagicSquare[3-k][3-j]) != 15) && j == size-1) {
+                // Check condiag
+                if (((ConDiagonal += magicSquare[magicSquareSize-1-k][magicSquareSize-1-j]) != numberSum) && j == magicSquareSize-1) {
 					printf("No magic happening...CD");
-					return 1;
+					return;
 				}
 				k++;
 			}
 
 			// Reset
-			if (j == size-1) {
+			if (j == magicSquareSize-1) {
 				Horizontal = 0;
 				Vertical = 0;
 				Diagonal = 0;
 				ConDiagonal = 0;
-				k = size;
+				k = magicSquareSize;
 			}
 		}
 	}
 	printf("Magic!");
-	return 0;
 }
 
+// Calculates the value the sum should have
+int CalculateNumberSum (int size) {
+    int NumberCount = size*size;
+    int NumberSum = 0;
+    int i;
+
+    for (i = 0; i < NumberCount; i++) {
+        NumberSum += NumberCount-i;
+    }
+    return (NumberSum/size);
+}
+
+// Builds the Square
+int **InitializeMagicSquare(int size) {
+    int **MagicSquare = (int **)malloc(size*sizeof(int *));
+    int *UsedNumbers = (int *)malloc(size*sizeof(int));
+    int i, j;
+
+    for (i = 0; i < size; i++) {
+        MagicSquare[i] = (int *)malloc(size * sizeof(int));
+    }
+
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
+            int RandomTemp = GetRandomNumber(size);
+            while (IsRandomNumberUsed(UsedNumbers, RandomTemp, size*size)) {
+                RandomTemp = GetRandomNumber(size);
+            }
+            MagicSquare[i][j] = RandomTemp;
+            UsedNumbers[i+j] = MagicSquare[i][j];
+        }
+    }
+    return MagicSquare;
+}
+
+// Draws the MagicSquare
+void DrawMagicSquare(int **magicSquare, int size) {
+    int i, j;
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
+            printf(". ");
+            printf("%d", magicSquare[i][j]);
+            printf(" .");
+        }
+        printf("\n");
+    }
+    return;
+}
+
+// Generates random number
+int GetRandomNumber(int size) {
+    time_t TimeStore;
+    time(&TimeStore);
+    srand((unsigned int)TimeStore);
+
+    return (rand()%(size*size))+1;
+}
+
+// Returns true if the generated randomNumber is already used
+bool IsRandomNumberUsed(int *usedNumbers, int randomNumber, int size) {
+    int i = 0;
+    for (i = 0; i < size; i++) {
+        if (usedNumbers[i] == randomNumber) {
+            return true;
+        }
+    }
+    return false;
+}
---
+++ main.h
@@ -0,0 +1,7 @@
+int main();
+int CalculateNumberSum (int);
+int **InitializeMagicSquare(int);
+void DrawMagicSquare(int **, int);
+int GetRandomNumber(int);
+bool IsRandomNumberUsed(int *, int, int);
+void CheckMagicSquare(int **, int, int);
